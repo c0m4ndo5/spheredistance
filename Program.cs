56cb0f697d04b229cef65cfbd713b41d86130915
YuriW <c0m4ndo45@gmail.com>
2019-09-18 19:49:25 -0600

    Initial project commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace spheredistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
